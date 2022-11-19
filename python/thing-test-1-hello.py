2c10d844a549a547ee662c82b61d0abfae94b155
Matthew Page <46349796+MatthewPageUK@users.noreply.github.com>
2022-11-19 01:12:51 +0000

    First code

---
+++ python/thing-test-1-hello.py
@@ -0,0 +1,50 @@
+#   __  __    __   ____  ____/ ___    ____  _   _  ____  _  _  ___
+#  (  \/  )  /__\ (_  _)(_  _)/ __)  (_  _)( )_( )(_  _)( \( )/ __)
+#   )    (  /(__)\  )(    )(  \__ \    )(   ) _ (  _)(_  )  (( (_-.
+#  (_/\/\_)(__)(__)(__)  (__) (___/   (__) (_) (_)(____)(_)\_)\___/
+#
+#  Raspberry Pi Pico-W IOT Sensor
+#
+#  Hello world test.
+#
+#  Prints Hello followed by a country name then flashes the
+#  onboard LED.
+#
+#  By           - Matthew Page
+#  Credit       - Everyone who came before and wrote a how to guide
+#  Version      - 0.1
+#  Release date - 21st November 2022
+#
+from sys import exit
+from time import sleep
+from machine import Pin
+try:
+    from thing_test import log, flash_led, splash
+except ImportError:
+    print("\nPlease save 'thing_test.py' file to your Thing before running the test\n\n")
+    exit(0)
+
+##############################################################################
+# Test settings
+##############################################################################
+WORLD = "Afghanistan, Albania, Algeria, Andorra, Angola, Antigua and Barbuda, Argentina, Armenia, Australia, Austria, Azerbaijan, Bahamas, Bahrain, Bangladesh, Barbados, Belarus, Belgium, Belize, Benin, Bhutan, Bolivia, Bosnia and Herzegovina, Botswana, Brazil, Brunei, Bulgaria, Burkina Faso, Burundi, Côte d'Ivoire, Cabo Verde, Cambodia, Cameroon, Canada, Central African Republic, Chad, Chile, China, Colombia, Comoros, Congo (Congo-Brazzaville), Costa Rica, Croatia, Cuba, Cyprus, Czechia (Czech Republic), Democratic Republic of the Congo, Denmark, Djibouti, Dominica, Dominican Republic, Ecuador, Egypt, El Salvador, Equatorial Guinea, Eritrea, Estonia, Eswatini, Ethiopia, Fiji, Finland, France, Gabon, Gambia, Georgia, Germany, Ghana, Greece, Grenada, Guatemala, Guinea, Guinea-Bissau, Guyana, Haiti, Holy See, Honduras, Hungary, Iceland, India, Indonesia, Iran, Iraq, Ireland, Israel, Italy, Jamaica, Japan, Jordan, Kazakhstan, Kenya, Kiribati, Kuwait, Kyrgyzstan, Laos, Latvia, Lebanon, Lesotho, Liberia, Libya, Liechtenstein, Lithuania, Luxembourg, Madagascar, Malawi, Malaysia, Maldives, Mali, Malta, Marshall Islands, Mauritania, Mauritius, Mexico, Micronesia, Moldova, Monaco, Mongolia, Montenegro, Morocco, Mozambique, Myanmar (formerly Burma), Namibia, Nauru, Nepal, Netherlands, New Zealand, Nicaragua, Niger, Nigeria, North Korea, North Macedonia, Norway, Oman, Pakistan, Palau, Palestine State, Panama, Papua New Guinea, Paraguay, Peru, Philippines, Poland, Portugal, Qatar, Romania, Russia, Rwanda, Saint Kitts and Nevis, Saint Lucia, Saint Vincent and the Grenadines, Samoa, San Marino, Sao Tome and Principe, Saudi Arabia, Senegal, Serbia, Seychelles, Sierra Leone, Singapore, Slovakia, Slovenia, Solomon Islands, Somalia, South Africa, South Korea, South Sudan, Spain, Sri Lanka, Sudan, Suriname, Sweden, Switzerland, Syria, Tajikistan, Tanzania, Thailand, Timor-Leste, Togo, Tonga, Trinidad and Tobago, Tunisia, Turkey, Turkmenistan, Tuvalu, Uganda, Ukraine, United Arab Emirates, United Kingdom, United States of America, Uruguay, Uzbekistan, Vanuatu, Venezuela, Vietnam, Yemen, Zambia, Zimbabwe"
+
+##############################################################################
+# Test setup
+##############################################################################
+
+# Setup the LED Pin
+led = Pin('LED', Pin.OUT)
+
+# Main program
+splash("Hello world test")
+
+##############################################################################
+# Run the test
+##############################################################################
+for country in WORLD.split(", "):
+    log("Hello ", country)
+    flash_led(led)
+    sleep(0.5)
+
+log("Finished")
---
+++ python/thing_test.py
@@ -0,0 +1,72 @@
+#   __  __    __   ____  ____/ ___    ____  _   _  ____  _  _  ___
+#  (  \/  )  /__\ (_  _)(_  _)/ __)  (_  _)( )_( )(_  _)( \( )/ __)
+#   )    (  /(__)\  )(    )(  \__ \    )(   ) _ (  _)(_  )  (( (_-.
+#  (_/\/\_)(__)(__)(__)  (__) (___/   (__) (_) (_)(____)(_)\_)\___/
+#
+#  Raspberry Pi Pico-W IOT Sensor
+#
+#  Additional functions and helpers for the test scripts.
+#  Mostly formatting and none essential things.
+#
+#  log(text1, text2)		Outputs log text
+#  flash_led(led)			Flashes the LED
+#  splash(title)			Displays a splash screen
+#
+#  By           - Matthew Page
+#  Credit       - Everyone who came before and wrote a how to guide
+#  Version      - 0.1
+#  Release date - 21st November 2022
+#
+from sys import exit
+from time import sleep
+
+def log(text1, text2 = None):
+    """Write some text out to the display
+
+    Parameters
+    ----------
+    text1
+        The text to print out
+    text2
+        Optional text to show in --[ here ]--
+    """
+    if text2 is not None:
+        print("| {t1:<25} --[ {t2:<16} ]--".format(
+            t1 = str(text1),
+            t2 = str(text2)))
+    else:
+        print("| {t1}".format(t1 = str(text1)))
+
+def flash_led(led: Pin):
+    """Flash the LED
+
+    Parameters
+    ----------
+    led : Pin
+        The LED Pin to use
+    """
+    try:
+        led.off()
+        for i in range(6):
+            led.toggle()
+            sleep(0.1)
+
+    except AttributeError as err:
+        log("Failed to control LED", err)
+        exit(0)
+
+def splash(title: str):
+    """Show a splash screen
+
+    Parameters
+    ----------
+    title : str
+        The splash screen title
+    """
+    print(" __  __    __   ____  ____/ ___    ____  _   _  ____  _  _  ___ ")
+    print("(  \/  )  /__\ (_  _)(_  _)/ __)  (_  _)( )_( )(_  _)( \( )/ __)")
+    print(" )    (  /(__)\  )(    )(  \__ \    )(   ) _ (  _)(_  )  (( (_-.")
+    print("(_/\/\_)(__)(__)(__)  (__) (___/   (__) (_) (_)(____)(_)\_)\___/")
+    print("________________________________________________________________")
+    print("|\n| {t}\n|".format(t = title))
+
